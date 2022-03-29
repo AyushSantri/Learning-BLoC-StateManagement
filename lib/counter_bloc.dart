592129db4224d5ccc709f9da52cd19b4e46ee6a3
AyushSantri <ayushsantri222@gmail.com>
2022-03-29 18:44:14 +0530

    created a streamController inside counter_bloc.dart

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,1 +1,8 @@
-class CounterBloc {}
+import 'dart:async';
+
+class CounterBloc {
+  /* StreamController is like a pipe and whatever comes in is Sink and
+  * whatever goes out is sink */
+
+  final stateStreamController = StreamController<int>();
+}
