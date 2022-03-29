c41534fce33b5e55e23383a82d53d9b0ca090dc1
AyushSantri <ayushsantri222@gmail.com>
2022-03-29 18:53:55 +0530

    privatised our stream controller

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -4,11 +4,11 @@ class CounterBloc {
   /* StreamController is like a pipe and whatever comes in is Sink (i.e, input) and
   * whatever goes out is stream (i.e, output)*/
 
-  final stateStreamController = StreamController<int>();
+  final _stateStreamController = StreamController<int>();
 
   //from StreamController we are asking for sink and datatype is StreamSink
-  StreamSink<int> get counterSink => stateStreamController.sink;
+  StreamSink<int> get counterSink => _stateStreamController.sink;
 
   //getting stream/output form streamController
-  Stream<int> get counterStream => stateStreamController.stream;
+  Stream<int> get counterStream => _stateStreamController.stream;
 }
