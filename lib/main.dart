f9230a79673befb426cbaeeb383b321ea3620f94
AyushSantri <ayushsantri222@gmail.com>
2022-03-30 02:14:00 +0530

    disposed the streamControllers in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
   final counterBloc = CounterBloc();
 
   @override
+  void dispose() {
+    counterBloc.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
