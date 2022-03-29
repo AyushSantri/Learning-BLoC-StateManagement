5fd6d2a89b799350e0953d55d72fbebc4ffc2430
AyushSantri <ayushsantri222@gmail.com>
2022-03-29 18:46:04 +0530

    edited comment in counter_bloc.dart

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 class CounterBloc {
-  /* StreamController is like a pipe and whatever comes in is Sink and
-  * whatever goes out is sink */
+  /* StreamController is like a pipe and whatever comes in is Sink (i.e, input) and
+  * whatever goes out is stream (i.e, output)*/
 
   final stateStreamController = StreamController<int>();
 }
